f6e07d970b64d9cca33c5a4c365f2bd44f222429
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-08-31 13:39:51 +0600

    setup continue

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+async function boostrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("database is connected successfully");
+    app.listen(config.port, () => {
+      console.log(`app is listening in the ${config.port}`);
+    });
+  } catch (err) {
+    console.log("Failed to connect database", err);
+  }
+}
+
+boostrap();
