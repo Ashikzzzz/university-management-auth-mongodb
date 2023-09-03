5c20b88b8e21c6d543bd9a118a17812fc473160a
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-09-04 01:37:30 +0600

    check husky

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import config from "./config/index";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("database is connected successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('database is connected successfully')
     app.listen(config.port, () => {
-      console.log(`app is listening in the ${config.port}`);
-    });
+      console.log(`app is listening in the ${config.port}`)
+    })
   } catch (err) {
-    console.log("Failed to connect database", err);
+    console.log('Failed to connect database', err)
   }
 }
 
-boostrap();
+boostrap()
