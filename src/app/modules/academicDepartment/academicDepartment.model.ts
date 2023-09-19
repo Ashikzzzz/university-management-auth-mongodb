badc399f34a17c7b6945f11ed0b7059baf74f84d
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-09-19 20:29:31 +0600

    create academic department model

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+import {
+  AcademicDepartmentModel,
+  IAcademicDepartment,
+} from './academicDepartment.interface';
+
+const academicDepartmentSchema = new Schema<IAcademicDepartment>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+// 3. Create a Model.
+export const AcademicDepartment = model<
+  IAcademicDepartment,
+  AcademicDepartmentModel
+>('AcademicDepartment', academicDepartmentSchema);
