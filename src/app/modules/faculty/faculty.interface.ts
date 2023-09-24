2da854f796d697aa8849cce97d3b0cae29ce6ae0
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-09-25 00:09:24 +0600

    create faculty interface

---
+++ src/app/modules/faculty/faculty.interface.ts
@@ -0,0 +1,27 @@
+import { Types } from 'mongoose';
+import { IAcademicDepartment } from '../academicDepartment/academicDepartment.interface';
+import { IAcademicFaculty } from '../academicFaculty/academicFaculty.interface';
+
+export type UserName = {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+};
+
+export type IFaculty = {
+  id: string;
+  name: UserName;
+  profileImage: string;
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  gender?: 'male' | 'female';
+  permanentAddress?: string;
+  presentAddress?: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+
+  academicDepartment: Types.ObjectId | IAcademicDepartment;
+  academicFaculty: Types.ObjectId | IAcademicFaculty;
+  designation: string;
+};
