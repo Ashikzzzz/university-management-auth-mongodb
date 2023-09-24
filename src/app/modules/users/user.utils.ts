695a195be8d61e6d4878c984fec126511198c305
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-09-25 02:03:01 +0600

    generate id for admin

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -42,3 +42,21 @@ export const generateFacultyId = async (): Promise<string> => {
   incrementId = `F-${incrementId}`;
   return incrementId;
 };
+
+// generate id for admin
+export const findLastAdminID = async (): Promise<string | undefined> => {
+  const lastAdmin = await User.findOne({ role: 'admin' }, { id: 1, _id: 0 })
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
+};
+
+export const generateAdminId = async (): Promise<string> => {
+  const currentId =
+    (await findLastFacultyID()) || (0).toString().padStart(5, '0');
+  let incrementId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+  incrementId = `A-${incrementId}`;
+  return incrementId;
+};
